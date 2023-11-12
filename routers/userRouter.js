191a714b7820e49576a31a197f32772864145121
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-11 17:59:48 -0800

    Created a post router for registration

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+
 const router = Router();
 
 router.get("/", (_, res) => {
   res.send("Welcome to users router!");
+});
+
+router.post("/register", (req, res) => {
+  res.send("Register user");
 });
 
 export default router;
