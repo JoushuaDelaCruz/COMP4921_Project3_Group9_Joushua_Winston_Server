20547c9a05c5d42b63d91195f20fff6b4f72d353
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-12 22:44:46 -0800

    Added the session and removed dotenv.config

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-// const session = require("express-session");
-// const mongoStore = require("connect-mongo");
-import dotenv from "dotenv";
+import session from "express-session";
+
+import "./configs/dotenvConfig.js";
+
 import { corsConfig } from "./configs/corsConfig.js";
+import { sessionConfig } from "./configs/sessionConfig.js";
 
 import userRouter from "./routers/userRouter.js";
 
 const app = express();
-dotenv.config();
 
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session(sessionConfig));
 
 app.use("/user", userRouter);
 
