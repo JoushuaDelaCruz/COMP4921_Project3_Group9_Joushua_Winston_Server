cd73e0370c6c0ce28c1ce1dc74aed14a88552ed7
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-22 01:44:57 -0800

    Moved verify to here and implemented express-session

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import expressSession from "express-session";
 
 import "./configs/dotenvConfig.js";
 import { corsConfig } from "./configs/corsConfig.js";
+import { sessionConfig } from "./configs/sessionConfig.js";
 
 import userRouter from "./routers/userRouter.js";
 import authRouter from "./routers/authRouter.js";
-import profileRouter from "./routers/profileRouter.js";
+// import profileRouter from "./routers/profileRouter.js";
 
 const app = express();
 
@@ -17,13 +19,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressSession(sessionConfig));
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
-app.use("/profile", profileRouter);
+// app.use("/profile", profileRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to our server!");
+});
+
+app.get("/verify", async (req, res) => {
+  const session = req.cookies.session;
+  if (session === undefined) {
+    res.send(false);
+    return;
+  }
+  req.sessionStore.get(session, (err, session) => {
+    if (err) {
+      console.log(err);
+      res.send(false);
+      return;
+    }
+    if (session === null) {
+      res.status(400).send(false);
+      return;
+    }
+    res.send(session.authenticated);
+  });
 });
 
 app.get("*", (req, res) => {
