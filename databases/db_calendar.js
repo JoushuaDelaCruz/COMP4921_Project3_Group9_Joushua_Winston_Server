13359a3377cae70088a2131ec82b94b93db104cc
Winston <winston_nguyenn@hotmail.com>
2023-12-03 02:03:23 -0800

    Fixed editing event friends

--- databases/db_calendar.js
+++ databases/db_calendar.js
@@ -1,28 +1,37 @@
 import database from "../mySQLDatabaseConnection.js";
 
 
-export const getEventsById = async (user_id) => {
+export const getEventsById = async (user_id, username) => {
   const query = `
-        SELECT 
-          title,
-          start_datetime,
-          end_datetime,
-          event_colour,
-          is_all_day,
-          start_timezone,
-          end_timezone,
-          location,
-          description,
-          recurrence_id,
-          recurrence_rule,
-          recurrence_exception,
-          uuid
-        FROM event
-        JOIN event_user USING (event_id)
-        WHERE user_id = :user_id
-        AND accepted = 1
+      SELECT 
+        title,
+        start_datetime,
+        end_datetime,
+        event_colour,
+        is_all_day,
+        start_timezone,
+        end_timezone,
+        location,
+        description,
+        recurrence_id,
+        recurrence_rule,
+        recurrence_exception,
+        uuid,
+        event_friends.friends,
+        username
+      FROM event
+      JOIN event_user USING (event_id)
+      JOIN users ON (event.original_user_id = users.user_id)
+      JOIN (SELECT GROUP_CONCAT(username) as friends, event_id
+        FROM event_user
+        JOIN users USING (user_id)
+        WHERE username != :username
+        GROUP BY(event_id)) as event_friends USING (event_id)
+      WHERE event_user.user_id = :user_id
+      AND accepted = 1
+      AND recycle_datetime IS NULL;
     `;
-  const params = { user_id };
+  const params = { user_id, username };
 
   const result = await database.query(query, params);
   return result[0];
@@ -41,15 +50,16 @@ export const createEvent = async (eventData) => {
   return result[0].insertId !== undefined;
 }
 
-
 export const deleteEvent = async (uuid, user_id) => {
+  const deleteDate = new Date();
+  deleteDate.setMonth(deleteDate.getMonth() + 1);
   const query = `
-      DELETE FROM event_user
-      WHERE event_id = (SELECT event_id FROM event WHERE uuid = :uuid)
-      AND user_id = :user_id
+      UPDATE event_user SET recycle_datetime = :delete_date
+      WHERE user_id = :user_id
+      AND event_id = (SELECT event_id FROM event WHERE uuid = :uuid);
     `;
 
-  const result = await database.query(query, {uuid: uuid, user_id: user_id});
+  const result = await database.query(query, {uuid: uuid, user_id: user_id, delete_date: deleteDate});
   return result[0].affectedRows !== 0;
 }
 
@@ -88,4 +98,62 @@ export const sendEventRequest = async (uuid, friends) => {
 
   return true;
   
+}
+
+export const removeEventRequest = async (uuid, friends) => {
+  const query = `
+    DELETE FROM event_user
+    WHERE event_id = (SELECT event_id FROM event WHERE uuid = :uuid)
+    AND user_id = (SELECT user_id FROM users WHERE username = :username);
+  `;
+
+  await friends.forEach(friend => {
+    try {
+      database.query(query, {uuid: uuid, username: friend});
+    } catch {
+      console.log("ERROR REMOVING EVENT REQUESTS");
+      return false;
+    }
+  }); 
+
+  return true;
+  
+}
+
+
+// Give it req.session.user_id, req.session.username
+export const getRecycleBin = async(user_id, username) => {
+  const query = `
+      SELECT 
+        title,
+        start_datetime,
+        end_datetime,
+        event_colour,
+        is_all_day,
+        start_timezone,
+        end_timezone,
+        location,
+        description,
+        recurrence_id,
+        recurrence_rule,
+        recurrence_exception,
+        uuid,
+        event_friends.friends,
+        username
+      FROM event
+      JOIN event_user USING (event_id)
+      JOIN users ON (event.original_user_id = users.user_id)
+      JOIN (SELECT GROUP_CONCAT(username) as friends, event_id
+        FROM event_user
+        JOIN users USING (user_id)
+        WHERE username != :username
+        GROUP BY(event_id)) as event_friends USING (event_id)
+      WHERE event_user.user_id = :user_id
+      AND accepted = 1
+      AND recycle_datetime IS NOT NULL;
+    `;
+  const params = { user_id, username };
+
+  const result = await database.query(query, params);
+  return result[0];
 }
--- routers/calendarRouter.js
+++ routers/calendarRouter.js
@@ -9,7 +9,10 @@ router.get("/", (_, res) => {
 
 router.get("/userEvents", async (req, res) => {
   if(req.session.authenticated) {
-    let results = await db_calendar.getEventsById(req.session.user_id);
+    let results = await db_calendar.getEventsById(req.session.user_id, req.session.username);
+    results.forEach((row) => {
+      row.is_original = row.username === req.session.username
+    })
     res.send({ events: results, success: true });
     return;
   }
@@ -20,10 +23,6 @@ router.get("/userEvents", async (req, res) => {
 router.post("/createEvent", async (req, res) => {
   if(req.session.authenticated) {
     const created_datetime = new Date();
-    // let end_datetime = new Date(req.body.end_datetime);
-    // if(req.body.is_all_day) {
-    //   end_datetime.setMinutes(end_datetime.getMinutes() - 1);
-    // }
     const eventData = {
       user_id: req.session.user_id,
       title: req.body.title,
@@ -56,6 +55,7 @@ router.post("/createEvent", async (req, res) => {
   }
 })
 
+
 router.post("/updateEvent", async (req, res) => {
   if(req.session.authenticated) {
     const created_datetime = new Date();
@@ -75,9 +75,24 @@ router.post("/updateEvent", async (req, res) => {
       uuid: req.body.uuid
     }
 
-
     const isSuccessful = await db_calendar.updateEvent(eventData);
     if (isSuccessful) {
+      const deleteFriends = req.body.original_friends.filter(value => !req.body.new_friends.includes(value));
+      const addFriends = req.body.new_friends.filter(value => !req.body.original_friends.includes(value));
+      if(addFriends) {
+        const success = db_calendar.sendEventRequest(req.body.uuid, addFriends);
+        if (!success) {
+          res.status(500).json({ message: "Error adding event requests" });
+          return;
+        }
+      }
+      if(deleteFriends) {
+        const success = db_calendar.removeEventRequest(req.body.uuid, deleteFriends);
+        if (!success) {
+          res.status(500).json({ message: "Error removing event requests" });
+          return;
+        }
+      }
       res.json({ success: true });
       return;
     }
@@ -101,8 +116,15 @@ router.post("/deleteEvent", async (req, res) => {
 })
 
 router.post("/sendEventRequest", async (req, res) => {
-  console.log(req.body.added_friends);
   const success = db_calendar.sendEventRequest(req.body.uuid, req.body.added_friends);
+  if (success) {
+    res.json({ success: true });
+    return;
+  }
+
+  res.status(500).json({ message: "Error sending event requests" });
+  return;
 })
+
 
 export default router;
