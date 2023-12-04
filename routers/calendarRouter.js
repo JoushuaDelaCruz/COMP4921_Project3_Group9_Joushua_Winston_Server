9ddd95546291b9ac2bd638b06ff98a3dd0e7caa4
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-12-03 16:34:26 -0800

    used cookie instead of session

--- routers/calendarRouter.js
+++ routers/calendarRouter.js
@@ -8,23 +8,44 @@ router.get("/", (_, res) => {
 });
 
 router.get("/userEvents", async (req, res) => {
-  if(req.session.authenticated) {
-    let results = await db_calendar.getEventsById(req.session.user_id, req.session.username);
+  const session = req.cookies.session;
+  req.sessionStore.get(session, async (err, session) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error getting events" });
+      return;
+    }
+    if (session === null) {
+      res.status(400).json({ message: "Invalid session" });
+      return;
+    }
+    const results = await db_calendar.getEventsById(
+      session.user_id,
+      session.username
+    );
     results.forEach((row) => {
-      row.is_original = row.username === req.session.username
-    })
+      row.is_original = row.username === session.username;
+    });
     res.send({ events: results, success: true });
     return;
-  }
-  res.status(401).json({ message: "Invalid session" });
-  return;
+  });
 });
 
 router.post("/createEvent", async (req, res) => {
-  if(req.session.authenticated) {
+  const session = req.cookies.session;
+  req.sessionStore.get(session, async (err, session) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error creating event" });
+      return;
+    }
+    if (session === null) {
+      res.status(400).json({ message: "Invalid session" });
+      return;
+    }
     const created_datetime = new Date();
     const eventData = {
-      user_id: req.session.user_id,
+      user_id: session.user_id,
       title: req.body.title,
       description: req.body.description,
       location: req.body.location,
@@ -36,31 +57,41 @@ router.post("/createEvent", async (req, res) => {
       recurrence_rule: req.body.recurrence_rule,
       created_datetime: created_datetime,
       event_colour: req.body.event_colour || "WIP",
-      uuid: req.body.uuid
-    }
-
+      uuid: req.body.uuid,
+    };
 
     const isSuccessful = await db_calendar.createEvent(eventData);
     if (isSuccessful) {
-      if (req.body.added_friends) {
-        res.redirect(307, '/calendar/sendEventRequest');
-        return;
-      } else {
-        res.json({ success: true });
+      const success = db_calendar.sendEventRequest(
+        req.body.uuid,
+        req.body.added_friends
+      );
+      if (!success) {
+        res.status(500).json({ message: "Error adding event requests" });
         return;
       }
+      res.json({ success: true });
+      return;
     }
     res.status(500).json({ message: "Error creating event" });
     return;
-  }
-})
-
+  });
+});
 
 router.post("/updateEvent", async (req, res) => {
-  if(req.session.authenticated) {
+  req.sessionStore.get(req.cookies.session, async (err, session) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error updating event" });
+      return;
+    }
+    if (session === null) {
+      res.status(400).json({ message: "Invalid session" });
+      return;
+    }
     const created_datetime = new Date();
     const eventData = {
-      user_id: req.session.user_id,
+      user_id: session.user_id,
       title: req.body.title,
       description: req.body.description,
       location: req.body.location,
@@ -72,22 +103,29 @@ router.post("/updateEvent", async (req, res) => {
       recurrence_rule: req.body.recurrence_rule,
       created_datetime: created_datetime,
       event_colour: req.body.event_colour || "WIP",
-      uuid: req.body.uuid
-    }
+      uuid: req.body.uuid,
+    };
 
     const isSuccessful = await db_calendar.updateEvent(eventData);
     if (isSuccessful) {
-      const deleteFriends = req.body.original_friends.filter(value => !req.body.new_friends.includes(value));
-      const addFriends = req.body.new_friends.filter(value => !req.body.original_friends.includes(value));
-      if(addFriends) {
+      const deleteFriends = req.body.original_friends.filter(
+        (value) => !req.body.new_friends.includes(value)
+      );
+      const addFriends = req.body.new_friends.filter(
+        (value) => !req.body.original_friends.includes(value)
+      );
+      if (addFriends) {
         const success = db_calendar.sendEventRequest(req.body.uuid, addFriends);
         if (!success) {
           res.status(500).json({ message: "Error adding event requests" });
           return;
         }
       }
-      if(deleteFriends) {
-        const success = db_calendar.removeEventRequest(req.body.uuid, deleteFriends);
+      if (deleteFriends) {
+        const success = db_calendar.removeEventRequest(
+          req.body.uuid,
+          deleteFriends
+        );
         if (!success) {
           res.status(500).json({ message: "Error removing event requests" });
           return;
@@ -98,25 +136,39 @@ router.post("/updateEvent", async (req, res) => {
     }
     res.status(500).json({ message: "Error updating event" });
     return;
-  }
-})
+  });
+});
 
 router.post("/deleteEvent", async (req, res) => {
-  if(req.session.authenticated) {
-    let results = await db_calendar.deleteEvent(req.body.uuid, req.session.user_id);
+  const session = req.cookies.session;
+  req.sessionStore.get(session, async (err, session) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error deleting event" });
+      return;
+    }
+    if (session === null) {
+      res.status(400).json({ message: "Invalid session" });
+      return;
+    }
+    const results = await db_calendar.deleteEvent(
+      req.body.uuid,
+      session.user_id
+    );
     if (results) {
       res.send({ success: true });
       return;
     }
     res.status(500).json({ message: "Error deleting event" });
     return;
-  }
-  res.status(401).json({ message: "Invalid session" });
-  return;
-})
+  });
+});
 
 router.post("/sendEventRequest", async (req, res) => {
-  const success = db_calendar.sendEventRequest(req.body.uuid, req.body.added_friends);
+  const success = db_calendar.sendEventRequest(
+    req.body.uuid,
+    req.body.added_friends
+  );
   if (success) {
     res.json({ success: true });
     return;
@@ -124,7 +176,6 @@ router.post("/sendEventRequest", async (req, res) => {
 
   res.status(500).json({ message: "Error sending event requests" });
   return;
-})
-
+});
 
 export default router;
