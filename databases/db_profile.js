28046205f22406582be35e91f1e16a0d3165189b
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-12-05 20:39:24 -0800

    Implemented the creation of groups

--- databases/db_profile.js
+++ databases/db_profile.js
@@ -63,6 +63,58 @@ export const searchFriends = async (current_user, name) => {
   }
 };
 
+export const createGroup = async (group_name, user_id) => {
+  const query = `
+  INSERT INTO freedb_DB_calendar.group (group_name, created_datetime, owner_user_id)
+  VALUES (:group_name, :date_created, :user_id);
+  `;
+  const params = { group_name, date_created: new Date(), user_id };
+  try {
+    const result = await database.query(query, params);
+    const group_id = result[0].insertId;
+    return group_id;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+export const addGroupMembers = async (group_id, friends) => {
+  const query = `
+  INSERT INTO user_group (group_id, user_id)
+  VALUES (:group_id, :user_id);
+  `;
+  try {
+    for (const friend of friends) {
+      const params = { group_id, user_id: friend.value };
+      await database.query(query, params);
+    }
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+export const searchAcceptedFriends = async (current_user, name) => {
+  const query = `
+  SELECT 
+    user_id as value,
+    username as label
+  FROM users 
+  JOIN friend ON requester_user_id = :current_user AND friend_user_id = user_id OR requester_user_id = user_id AND friend_user_id = :current_user AND accepted = 1
+  WHERE username LIKE :name AND user_id != :current_user`;
+
+  const params = { current_user, name: `%${name}%` };
+  try {
+    const result = await database.query(query, params);
+    return result[0];
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
+
 export const getFriends = async (current_user) => {
   const query = `
   SELECT 
--- routers/profileRouter.js
+++ routers/profileRouter.js
@@ -7,6 +7,54 @@ router.get("/", (_, res) => {
   res.send("Welcome to profile router!");
 });
 
+router.post("/createGroup", async (req, res) => {
+  const session = req.cookies.session;
+  req.sessionStore.get(session, async (err, session) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error creating group" });
+      return;
+    }
+    if (session === null) {
+      res.status(400).json({ message: "Invalid session" });
+      return;
+    }
+    const group_name = req.body.group_name;
+    const group_members = req.body.members;
+    const user_id = session.user_id;
+    const group_id = await db_profile.createGroup(group_name, user_id);
+    if (group_id === null) {
+      res.status(500).json({ message: "Error creating group" });
+      return;
+    }
+    const result = await db_profile.addGroupMembers(group_id, group_members);
+    if (result) {
+      res.status(200).send(true);
+      return;
+    }
+    res.status(500).send(false);
+  });
+});
+
+router.get("/searchAcceptedFriends", async (req, res) => {
+  const session = req.cookies.session;
+  const name = req.query.name;
+  req.sessionStore.get(session, async (err, session) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error searching users");
+      return;
+    }
+    if (session === null) {
+      res.status(400).send("No session found");
+      return;
+    }
+    const user_id = session.user_id;
+    const result = await db_profile.searchAcceptedFriends(user_id, name);
+    res.status(200).send(result);
+  });
+});
+
 router.get("/friends", (req, res) => {
   const session = req.cookies.session;
   req.sessionStore.get(session, async (err, session) => {
